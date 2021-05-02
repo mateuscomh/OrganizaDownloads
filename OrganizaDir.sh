2e28e88a1d17f49339ebfc999c58fc77752a8e67
mateuscomh <3mhenrique@gmail.com>
2021-05-02 02:11:10 -0300

    otimizacao e melhora de identacao

--- OrganizaDir.sh
+++ OrganizaDir.sh
@@ -3,74 +3,57 @@
 #-----------HEAD-------------------------------------------------------------->
 # AUTOR             : Matheus Martins 3mhenrique@gmail.com
 # HOMEPAGE          : https://github.com/mateuscomh 
-# DATA CRIA«√O      : 
+# DATA CRIA√á√ÉO      : 
 # PROGRAMA          : Shell-Base
-# VERS√O            : 1.0.0
-# LICEN«A           : GPL3
-# PEQUENA-DESCRI«√O : Template padr„o/base para aplicaÁıes pessoais.
+# VERS√ÉO            : 1.0.0
+# LICEN√áA           : GPL3
+# PEQUENA-DESCRI√á√ÉO : Template padrÔøΩo/base para aplica√ß√µes pessoais.
 #
 # CHANGELOG         : 30/04/2021 12:00 - Criado script sob demanda de organizar
 #                     01/05/2021 21:00 - Envio para repo no github
-#                     - Adicionados coment·rios e identaÁ„o
+#                     - Adicionados coment√°rios e identa√ß√£o
+#                     - Reduzidas condicoes de valida√ß√£o de pastas
 #-----------END-HEAD-----------------------------------------------------------<
 
 #-----------VAR---------------------------------------------------------------->
 caminho="~/Downloads"
 #----------END-VAR-------------------------------------------------------------<
 
-#----------FUNC---------------------------------------------------------------->
-#----------END-FUNC------------------------------------------------------------<
+#-----------FUNC--------------------------------------------------------------->
+#-----------END-FUNC-----------------------------------------------------------<
 
 #Validar a entrada para o caminho a ser organizado.
 [[ -z $caminho ]] && echo "Caminho vazio" && exit 1
 
-#Forca o shell a ir atÈ a pasta informada em $caminho
+#Forca o shell a ir at√© a pasta informada em $caminho
+#Valida se caminho √© acessivel
 cd "$caminho"
+[[ $? == 1  ]] && echo "Caminho inexistente" && exit 1
 
 #Condicoes para validar existencia das pastas de organizar
-#Caso n„o tenha, ser· criada, caso tenha return 0
-if [ -d $caminho/Audio ];then
-  return 0
-else
- mkdir $caminho/Audio
-fi
-if [ -d $caminho/Variados ];then
-  echo exite > /dev/null
-else
- mkdir $caminho/Variados 
-fi
-if [ -d $caminho/Imagens ];then
-  echo exite > /dev/null
-else
- mkdir $caminho/Imagens
-fi
-if [ -d $caminho/Video ];then
-  echo exite > /dev/null
-else
- mkdir $caminho/Video 
-fi
-if [ -d $caminho/Docs ];then
-  echo exite > /dev/null
-else
- mkdir $caminho/Docs 
-fi
+[[ -d $caminho/Audio ]] || mkdir $$caminho/Audio
+[[ -d $caminho/Variados ]] || mkdir $caminho/Variados 
+[[ -d $caminho/Imagens ]] || mkdir $caminho/Imagens
+[[ -d $caminho/Video ]] || mkdir $caminho/Video 
+[[ -d $caminho/Docs ]] || mkdir $caminho/Docs 
 
-for file in *; do
-  if [[ $file == *.png || $file == *.jpg || $file == *.jpeg ]]; then
-    mv "$file" "$caminho/Imagens"
+  for file in *; do
+    if [[ $file == *.png || $file == *.jpg || $file == *.jpeg ]]; then
+      mv "$file" "$caminho/Imagens"
 
-  elif [[ $file == *.mp4 ]]; then
-    mv "$file" "$caminho/Imagens"
+    elif [[ $file == *.mp4 ]]; then
+      mv "$file" "$caminho/Videos"
 
-  elif [[ $file == *.mp3 || $file == *.ogg ]]; then
-    mv "$file" "$caminho/Audio"
+    elif [[ $file == *.mp3 || $file == *.ogg ]]; then
+      mv "$file" "$caminho/Audio"
 
-  elif [[ $file == *.mobi || $file == *.pdf || $file == *.srt ]];then
-    mv "$file" "$caminho/Docs"
+    elif [[ $file == *.mobi || $file == *.pdf || $file == *.srt ]];then
+      mv "$file" "$caminho/Docs"
 
-  elif [[ $file == *.* ]];then
-    mv "$file" "$caminho/Variados"
-  fi
+    elif [[ $file == *.* ]];then
+      mv "$file" "$caminho/Variados"
+   fi
 
-done
+  done
+
 exit 0
