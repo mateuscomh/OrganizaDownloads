447280de3cb867dfd056499cf27990d3d83f4008
Djangoo <60662558+mateuscomh@users.noreply.github.com>
2021-07-16 11:39:39 -0300

    Update na validação de pasta e arquivos
    - Adicionadas mais extensões de arquivos;
    - Adicionada condição de checagem na pasta;
    - Segregada validação como função.

--- OrganizaDir.sh
+++ OrganizaDir.sh
@@ -10,10 +10,13 @@
 # PEQUENA-DESCRIÇÃO : Script para organizar pasta (Downloads em especial) 
 #                     categorizando os arquivos para suas respectivas pastas.
 #
-# CHANGELOG         : 30/04/2021 12:00 - Criado script sob demanda de organizar
-#                     01/05/2021 21:00 - Envio para repo no github
-#                     - Adicionados comentários e identação
-#                     - Reduzidas condicoes de validação de pastas
+# CHANGELOG         : 30/04/2021 12:00 - Criado script sob demanda de organizar.
+#                     01/05/2021 21:00 - Envio para repo no github;
+#                     - Adicionados comentários e identação;
+#                     - Reduzidas condicoes de validação de pastas.
+#                     16/07/2021 11:00 - Adicionadas mais extensões de arquivos;
+#                     - Adicionada condição de checagem na pasta;
+#                     - Segregada validação como função.
 #-----------END-HEAD-----------------------------------------------------------<
 
 #-----------VAR---------------------------------------------------------------->
@@ -21,15 +24,24 @@ caminho="/home/salaam/Downloads"
 #----------END-VAR-------------------------------------------------------------<
 
 #-----------FUNC--------------------------------------------------------------->
-#-----------END-FUNC-----------------------------------------------------------<
-
+valida_caminho{
+#Confirmar caminho da pasta a ser organizada
+  read -p "O caminho informado é: $caminho " -d' confirma (y)/(n)?' PERGUNTA; echo
+  if [[ $PERGUNTA == n || $PERGUNTA == N ]] then
+    exit 1
+  fi
+  
 #Validar a entrada para o caminho a ser organizado.
-[[ -z $caminho ]] && echo "Caminho vazio" && exit 1
-
+  [[ -z $caminho ]] && echo "Caminho vazio" && exit 1
+  
 #Forca o shell a ir até a pasta informada em $caminho
+  cd "$caminho"
+
 #Valida se caminho é acessivel
-cd "$caminho"
-[[ $? == 1  ]] && echo "Caminho inexistente" && exit 1
+  [[ $? == 1  ]] && echo "Caminho inexistente" && exit 1
+ }
+#-----------END-FUNC-----------------------------------------------------------<
+valida_caminho
 
 #Condicoes para validar existencia das pastas de organizar
 [[ -d $caminho/Audio ]] || mkdir $$caminho/Audio
@@ -40,16 +52,16 @@ cd "$caminho"
 
 #Movimentacoes dos arquivos para suas respectivas pastas
   for file in *; do
-    if [[ $file == *.png || $file == *.jpg || $file == *.jpeg ]]; then
+    if [[ $file == *.png || $file == *.jpg || $file == *.jpeg || $file == *.tif || $file == *.tiff || $file == *.gif ]]; then
       mv "$file" "$caminho/Imagens"
 
-    elif [[ $file == *.mp4 ]]; then
+    elif [[ $file == *.mp4 || $file == *.mov || $file == *.avi || $file == *.mpeg || $file == *.mpv || $file == *.mpg ]]; then
       mv "$file" "$caminho/Video"
 
-    elif [[ $file == *.mp3 || $file == *.ogg ]]; then
+    elif [[ $file == *.mp3 || $file == *.ogg || $file == *.aac || $file == *.wav ]]; then
       mv "$file" "$caminho/Audio"
 
-    elif [[ $file == *.mobi || $file == *.pdf || $file == *.srt ]];then
+    elif [[ $file == *.mobi || $file == *.pdf || $file == *.srt || $file == *.py || $file == *.sh ]];then
       mv "$file" "$caminho/Docs"
 
     elif [[ $file == *.* ]];then
