9b5fea80b6e7dfbd97731927d89c6c144a7a6825
mateuscomh <3mhenrique@gmail.com>
2021-10-01 21:14:09 -0300

    Ajustes de sintaxe e modo verboso

--- OrganizaDir.sh
+++ OrganizaDir.sh
@@ -10,19 +10,20 @@
 # PEQUENA-DESCRIÇÃO : Script para organizar pasta (Downloads em especial) 
 #                     categorizando os arquivos para suas respectivas pastas.
 #
-# CHANGELOG         : 30/04/2021 12:00 - Criado script sob demanda de organizar.
-#                     01/05/2021 21:00 - Envio para repo no github;
-#                     - Adicionados comentários e identação;
-#                     - Reduzidas condicoes de validação de pastas.
-#                     16/07/2021 11:00 - Adicionadas mais extensões de arquivos;
-#                     - Adicionada condição de checagem na pasta;
-#                     - Segregada validação como função.
-#                     19/07/2021 10:00 - Aprimorada a chegagem do caminho e 
-# ajustada a alternativa "não"
-#                     21/07/2021 11:00 - Ajustado caminho para ser aplicado em 
-# vários usuarios
-#                     15/08/2021 19:20 - Script com função de organizar a pasta
-# de execução no momento
+# CHANGELOG         :
+#   30/04/2021 12:00 - Criado script sob demanda de organizar.
+#   01/05/2021 21:00 - Envio para repo no github;
+#     - Adicionados comentários e identação;
+#     - Reduzidas condicoes de validação de pastas.
+#   16/07/2021 11:00 - Adicionadas mais extensões de arquivos;
+#     - Adicionada condição de checagem na pasta;
+#     - Segregada validação como função.
+#   19/07/2021 10:00 - Aprimorada a chegagem do caminho e ajustada multi escolha
+#   21/07/2021 11:00 - Ajustado caminho para ser aplicado em vários usuarios
+#   15/08/2021 19:20 - Script com função de organizar a pasta de execução no momento
+#   01/10/2021 21:00 - Realizados ajustes de sintaxe padrão shell
+#     - Adicionado modo verboso para criação de pastas e mv de arquivos 
+#
 #-----------END-HEAD-----------------------------------------------------------<
 
 #-----------VAR---------------------------------------------------------------->
@@ -36,46 +37,45 @@ valida_caminho(){
   [[ -z $caminho ]] && echo "Caminho vazio" && exit 1
   
 #Forca o shell a ir até a pasta informada em $caminho
-  cd "$caminho"
+  cd "$caminho" || exit
 
 #Valida se caminho é acessivel
   [[ $? == 1 ]] && echo "Caminho inexistente" && exit 1
  }
 #-----------END-FUNC-----------------------------------------------------------<
 while true; do
-    read -p "Confirme o caminho: $caminho "  YN; echo
+    read -r -p "Pasta a ser organizada sera $caminho ? [Y/N]"  YN; echo
     case $YN in
       [Yy]* ) break;;
-      [Nn]* ) echo "Informe a pasta a ser organizada: "; read caminho
+      [Nn]* ) echo "Informe a pasta a ser organizada: "; read -r caminho
               valida_caminho            
               break;;
       *     ) echo "Por favor confirme com Y ou N";;
     esac
   done
 
-#Condicoes para validar existencia das pastas de organizar
-[[ -d $caminho/Audio ]] || mkdir $$caminho/Audio
-[[ -d $caminho/Variados ]] || mkdir $caminho/Variados 
-[[ -d $caminho/Imagens ]] || mkdir $caminho/Imagens
-[[ -d $caminho/Video ]] || mkdir $caminho/Video 
-[[ -d $caminho/Docs ]] || mkdir $caminho/Docs 
+#Condicoes para validar existencia das pastas de organizar[[ -d $caminho/Audio ]] || mkdir "$caminx"/Audio
+[[ -d $caminho/Audio ]] || mkdir -v "$caminho/Audio"
+[[ -d $caminho/Imagens ]] || mkdir -v "$caminho/Imagens"
+[[ -d $caminho/Video ]] || mkdir -v "$caminho/Video"
+[[ -d $caminho/Docs ]] || mkdir -v "$caminho/Docs"
 
 #Movimentacoes dos arquivos para suas respectivas pastas
   for file in *; do
     if [[ $file == *.png || $file == *.jpg || $file == *.jpeg || $file == *.tif || $file == *.tiff || $file == *.gif ]]; then
-      mv "$file" "$caminho/Imagens"
+      mv -i -v "$file" "$caminho/Imagens"
 
     elif [[ $file == *.mp4 || $file == *.mov || $file == *.avi || $file == *.srt || $file == *.mpeg || $file == *.mkv || $file == *.mpg ]]; then
-      mv "$file" "$caminho/Video"
+      mv  -i -v "$file" "$caminho/Video"
 
     elif [[ $file == *.mp3 || $file == *.ogg || $file == *.aac || $file == *.wav ]]; then
-      mv "$file" "$caminho/Audio"
+      mv  -i -v "$file" "$caminho/Audio"
 
     elif [[ $file == *.mobi || $file == *.pdf || $file == *.txt || $file == *.py || $file == *.sh || $file == *.xls || $file == *.doc || $file == *.ppt || $file == *.epub  ]]; then
-      mv "$file" "$caminho/Docs"
+      mv  -i -v "$file" "$caminho/Docs"
 
     elif [[ $file == *.* ]];then
-      mv "$file" "$caminho/Variados"
+      mv  -i -v "$file" "$caminho/Variados"
    fi
   done
 exit 0
