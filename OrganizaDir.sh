05238824dd61b10b27fbf86458469bd097e8bcb8
mateuscomh <3mhenrique@gmail.com>
2021-10-11 21:58:43 -0300

    Ajuste nas condicoes de mover em pastas

--- OrganizaDir.sh
+++ OrganizaDir.sh
@@ -23,13 +23,11 @@
 #   15/08/2021 19:20 - Script com função de organizar a pasta de execução no momento
 #   01/10/2021 21:00 - Realizados ajustes de sintaxe padrão shell
 #     - Adicionado modo verboso para criação de pastas e mv de arquivos 
-#
+#   11/10/2021 22:00 - Alterado bloco de condicoes ( melhoria de codigo)
 #-----------END-HEAD-----------------------------------------------------------<
-
 #-----------VAR---------------------------------------------------------------->
 caminho="$(pwd)"
 #----------END-VAR-------------------------------------------------------------<
-
 #-----------FUNC--------------------------------------------------------------->
 valida_caminho(){
 #Confirmar caminho da pasta a ser organizada
@@ -59,23 +57,21 @@ while true; do
 [[ -d $caminho/Imagens ]] || mkdir -v "$caminho/Imagens"
 [[ -d $caminho/Video ]] || mkdir -v "$caminho/Video"
 [[ -d $caminho/Docs ]] || mkdir -v "$caminho/Docs"
+[[ -d $caminho/Variados ]] || mkdir -v "$caminho/Variados"
 
 #Movimentacoes dos arquivos para suas respectivas pastas
   for file in *; do
-    if [[ $file == *.png || $file == *.jpg || $file == *.jpeg || $file == *.tif || $file == *.tiff || $file == *.gif ]]; then
-      mv -i -v "$file" "$caminho/Imagens"
-
-    elif [[ $file == *.mp4 || $file == *.mov || $file == *.avi || $file == *.srt || $file == *.mpeg || $file == *.mkv || $file == *.mpg ]]; then
-      mv  -i -v "$file" "$caminho/Video"
-
-    elif [[ $file == *.mp3 || $file == *.ogg || $file == *.aac || $file == *.wav ]]; then
-      mv  -i -v "$file" "$caminho/Audio"
-
-    elif [[ $file == *.mobi || $file == *.pdf || $file == *.txt || $file == *.py || $file == *.sh || $file == *.xls || $file == *.doc || $file == *.ppt || $file == *.epub  ]]; then
-      mv  -i -v "$file" "$caminho/Docs"
-
-    elif [[ $file == *.* ]];then
-      mv  -i -v "$file" "$caminho/Variados"
-   fi
+    case $file in
+	  	*.png|*.jpg|*.jpeg|*.tiff|*.gif)
+      		mv -i -v "$file" "$caminho/Imagens" ;; 
+		*.mp4|*.mov|*.avi|*.srt|*.mpeg|*.mkv|*.mpg)
+      		mv -i -v "$file" "$caminho/Video" ;;
+		*.mp3|*.ogg|*.aac|*.wav|*.wma) 
+      		mv -i -v "$file" "$caminho/Audio" ;;
+		*.pdf|*.txt|*.py|*.doc*|*.txt|*.epub|*.sh|*.xls*|*.ppt*) 
+      		mv -i -v "$file" "$caminho/Docs" ;;
+		*.*)
+      		mv -i -v "$file" "$caminho/Variados" ;;
+	esac
   done
 exit 0
